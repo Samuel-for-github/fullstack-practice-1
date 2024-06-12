45c2aba7b824d62bff5e8ca6a6fedc8612872610
Samuel-for-github <134806257+Samuel-for-github@users.noreply.github.com>
2024-06-12 14:24:41 +0530

    initial commit 4

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import appRouter from "./app.router.js";
-
+import cors from "cors";
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 app.use("/",appRouter)
 
 
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 import {app} from "./app.js";
+require('dotenv').config({
+    path: "./env"
+})
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log("Server started on port " + process.env.PORT || 3000);
+
+app.listen(process.env.PORT || 3001, ()=>{
+    console.log("Server started on port " + process.env.PORT || 3001);
 });
